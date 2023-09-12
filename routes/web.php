0c3b066f662b90af8209bae0693e8066c0cee4ad
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-09-12 09:51:35 +0700

    add: route for project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::group(['prefix' => 'projects'], function () {
+    // Menampilkan daftar project
+    Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
+    // Menampilkan form untuk membuat project baru
+    Route::get('/create', [ProjectController::class, 'create'])->name('projects.create');
+    // Menyimpan project baru ke dalaFm database
+    Route::post('/', [ProjectController::class, 'store'])->name('projects.store');
+    // Menampilkan detail project
+    Route::get('/detail/{id}', [ProjectController::class, 'show'])->name('projects.show');
+
+    // Menampilkan form untuk mengedit project
+    Route::get('/{id}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+
+    // Mengupdate project ke dalam database
+    Route::put('/{id}', [ProjectController::class, 'update'])->name('projects.update');
+
+    // Menghapus project dari database
+    Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
 
 Route::get('/', function () {
     return view('testPage.index');
@@ -28,4 +49,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::post('project/store', [ProjectController::class, 'store'])->name('project.store');
+
+require __DIR__ . '/auth.php';
