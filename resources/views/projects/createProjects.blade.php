e4925936612ea9ba6f3dca0f11fb1515e87a27a8
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-09-18 14:52:05 +0700

    tambah atribut input form baru

--- resources/views/projects/createProjects.blade.php
+++ resources/views/projects/createProjects.blade.php
@@ -13,27 +13,7 @@
                         <div class="card-body">
                             <form action="projects.store" method="post" class="parsley-examples" novalidate="">
                                 @csrf
-                                <div class="mb-3">
-                                    <label for="emailAddress" class="form-label">Project Name<span
-                                            class="text-danger">*</span></label>
-                                    <input type="email" name="email" parsley-trigger="change" required=""
-                                        placeholder="Enter email" class="form-control" id="emailAddress">
-                                    @error('label')
-                                    <p class="text-danger">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                                <div class="mb-3">
-                                    <label for="userName" class="form-label">Customers<span
-                                            class="text-danger">*</span></label>
-                                    <input type="text" name="nick" parsley-trigger="change" required=""
-                                        placeholder="Enter user name" class="form-control" id="userName">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="userName" class="form-label">Customers Contact Name<span
-                                            class="text-danger">*</span></label>
-                                    <input type="text" name="nick" parsley-trigger="change" required=""
-                                        placeholder="Enter user name" class="form-control" id="userName">
-                                </div>
+
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3">
@@ -52,35 +32,52 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
-                                            <label for="userName" class="form-label">Sales Oder Number(Optional)</label>
+                                <div class="row mb-3">
+                                    <div class="col-md-3">
+                                        <div>
+                                            <label for="userName" class="form-label">Memo Number</label>
                                             <input type="text" name="nick" parsley-trigger="change" required=""
                                                 placeholder="S1" class="form-control" id="userName">
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
+                                    <div class="col-md-2">
+                                        <div>
                                             <label for="userName" class="form-label">&nbsp;</label>
                                             <input type="text" name="nick" parsley-trigger="change" required=""
                                                 placeholder="23" class="form-control" id="userName">
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="mb-3">
+                                    <div class="col-md-3">
+                                        <div>
                                             <label for="userName" class="form-label">&nbsp;</label>
                                             <input type="text" name="nick" parsley-trigger="change" required=""
                                                 placeholder="Sales Order Code" class="form-control" id="userName">
                                         </div>
                                     </div>
+                                    <div class="col-md-2">
+                                        <div>
+                                            <label for="userName" class="form-label">&nbsp;</label>
+                                            <input type="text" name="nick" parsley-trigger="change" required=""
+                                                placeholder="Sales Order Code" class="form-control" id="userName">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div>
+                                            <label for="userName" class="form-label">&nbsp;</label>
+                                            <input type="text" name="nick" parsley-trigger="change" required=""
+                                                placeholder="Sales Order Code" class="form-control" id="userName">
+                                        </div>
+                                    </div>
+                                    <span class="help-block"><small>(Optional Jika PO Number Sudah Diisi)</small></span>
                                 </div>
+
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="mb-3">
-                                            <label for="userName" class="form-label">Memo Number</label>
+                                            <label for="userName" class="form-label">Sales Oder Number</label>
                                             <input type="text" name="nick" parsley-trigger="change" required=""
                                                 placeholder="S1" class="form-control" id="userName">
+                                            <span class="help-block"><small>(Optional)</small></span>
                                         </div>
                                     </div>
                                     <div class="col-md-4">
@@ -99,10 +96,29 @@
                                     </div>
                                 </div>
                                 <div class="mb-3">
+                                    <label for="emailAddress" class="form-label">Project Name<span
+                                            class="text-danger">*</span></label>
+                                    <input type="email" name="email" parsley-trigger="change" required=""
+                                        placeholder="Masukkan Project Name" class="form-control" id="emailAddress">
+                                    @error('label')
+                                    <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="userName" class="form-label">Customers<span
+                                            class="text-danger">*</span></label>
+                                    <select id="customers" class="form-control" name="customers[]"
+                                        style="color: black;">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
                                     <label for="userName" class="form-label">Customers Contact Name<span
                                             class="text-danger">*</span></label>
-                                    <input type="text" name="nick" parsley-trigger="change" required=""
-                                        placeholder="Enter user name" class="form-control" id="userName">
+                                    <select id="customers-name" class="form-control" name="customers-name[]"
+                                        style="color: black;">
+                                        <option value=""></option>
+                                    </select>
                                 </div>
                                 <div class="mb-3">
                                     <label for="project_manager" class="form-label">Project Manager<span
@@ -133,6 +149,12 @@
                                     </select>
                                 </div>
                                 <div class="mb-3">
+                                    <label for="userName" class="form-label">Location<span
+                                            class="text-danger">*</span></label>
+                                    <input type="text" name="" parsley-trigger="change" required=""
+                                        placeholder="Enter user name" class="form-control" id="userName">
+                                </div>
+                                <div class="mb-3">
                                     <label for="userName" class="form-label">Start Date<span
                                             class="text-danger">*</span></label>
                                     <input type="date" name="nick" parsley-trigger="change" required=""
@@ -152,6 +174,12 @@
                                 </div>
                                 <div class="mb-3">
                                     <label for="userName" class="form-label">Purchase Order Amount<span
+                                            class="text-danger">*</span></label>
+                                    <input type="text" name="nick" parsley-trigger="change" required=""
+                                        placeholder="Enter user name" class="form-control" id="userName">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="userName" class="form-label">Expense Budget<span
                                             class="text-danger">*</span></label>
                                     <input type="text" name="nick" parsley-trigger="change" required=""
                                         placeholder="Enter user name" class="form-control" id="userName">
@@ -185,5 +213,25 @@
                 icon: "success",
             });
     }
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('#customers').select2({
+            allowClear: true,
+            tags: true,
+            theme: 'classic',
+        });
+    });
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('#customers-name').select2({
+            allowClear: true,
+            tags: true,
+            theme: 'classic',
+        });
+    });
 </script>
 @endsection
