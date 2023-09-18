71aabcbc417939ab328c9958ac6762ba605a9bf4
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-09-18 10:13:09 +0700

    Test milestone

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -8,29 +8,30 @@ use Illuminate\Http\Request;
 
 class MilestoneController extends Controller
 {
-    public function store(Request $request, $projectId)
+    public function store(Request $request)
     {
+        $projectId = '9a22dce4-69f9-44bf-9eb2-189a495e10a4';
         // Validasi data input dari form
         $validatedData = $request->validate([
-            'entry_date' => 'required|date',
+            'submitted_date' => 'required|date',
             'description' => 'required|string',
             'due_date' => 'required|date',
             'progress' => 'required|string',
-            'file' => 'required|file|mimes:pdf,doc,docx|max:2048',
+            // Jangan lupa menambahkan validasi lainnya sesuai kebutuhan Anda
         ]);
-
-        // Simpan file ke penyimpanan (misalnya, penyimpanan lokal)
-        $uploadedFile = $request->file('file');
-        $fileExtension = $uploadedFile->getClientOriginalExtension();
-        $fileName = 'milestone_' . time() . '.' . $fileExtension;
-        $filePath = $uploadedFile->storeAs('milestones', $fileName); // Simpan file dalam direktori "milestones"
 
         // Simpan data Milestone ke dalam database dengan mengaitkannya dengan ID proyek
         $project = Project::findOrFail($projectId);
         $milestone = new Milestone($validatedData);
-        $milestone->file_path = $filePath; // Simpan path file ke dalam kolom "file_path" (sesuaikan dengan struktur database Anda)
         $project->milestones()->save($milestone);
 
-        return redirect()->route('projects.index', $projectId)->with('success', 'Milestone berhasil ditambahkan');
+        return dd("Berhasil");
+    }
+
+
+
+    public function create()
+    {
+        return view('testform');
     }
 }
