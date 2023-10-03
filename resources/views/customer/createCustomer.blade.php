c2a8580a0311af01630e6f4795e9293726056006
Muhammad Asyam Dirhamsyah <muhammadasyad173@gmail.com>
2023-10-03 13:48:03 +0700

    tambah customer

--- resources/views/customer/createCustomer.blade.php
+++ resources/views/customer/createCustomer.blade.php
@@ -3,17 +3,7 @@
 @section('content')
 <div class="content-page">
     <div class="content">
-
-        <!-- Start Content-->
         <div class="container-fluid">
-            <div class="content">
-                <!-- Start Content-->
-                <div class="container-fluid">
-                    <!-- end row -->
-                </div> <!-- container-fluid -->
-            </div>
-            {{-- @foreach ($customer as $name) --}}
-            {{-- <td>{{$name['companyName']}}</td> --}}
             <div class="row mt-1">
                 <div class="col">
                     <div class="card">
@@ -22,20 +12,18 @@
                                 @csrf
                                 <div class="mb-3">
                                     <label for="companyName" class="form-label">Company Name</label>
-                                    <input type="text" class="form-control" id="companyName" aria-describedby="companyNameHelp">
+                                    <input type="text" class="form-control" id="companyName" name="companyName" aria-describedby="companyNameHelp">
                                 </div>
                                 <div class="text-end">
-                                    <a href="{{ url('customer') }}">
-                                        <button type="button" class="btn btn-secondary waves-effect">Cancel</button>
-                                    </a>
+                                    <a href="{{ route('customer.index') }}" class="btn btn-secondary waves-effect">Cancel</a>
                                     <button class="btn btn-save waves-effect waves-light px-4" type="submit" id="submitButton">Save</button>
                                 </div>
                             </form>
-                            {{-- @endforeach --}}
                         </div>
                     </div>
-
-
-                </div> <!-- content -->
+                </div>
             </div>
-            @endsection
+        </div>
+    </div>
+</div>
+@endsection
