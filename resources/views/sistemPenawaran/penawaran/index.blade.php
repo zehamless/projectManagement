6445a3a10cb30f696bd8af2b4853b5c842492443
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-10-23 15:44:00 +0700

    tambah halaman index dari tiap menu

---
+++ resources/views/sistemPenawaran/penawaran/index.blade.php
@@ -0,0 +1,21 @@
+@extends('sistemPenawaran.template.index')
+
+@section('content')
+
+<div class="content-page">
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <h1>Halaman Penawaran</h1>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('pageScript')
+@endsection
